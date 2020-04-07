73939533462f51964db5f65dd3d4707651e46c7f
akl940 <59671045+akl940@users.noreply.github.com>
2020-04-07 15:54:21 -0400

    renamed files

--- ChattyChatChatClient.java
+++ ChattyChatChatClient.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*; 
 import java.util.Scanner; 
 
-public class ChattyClient {
+public class ChattyChatChatClient {
 
 	public static void main(String args[]) throws UnknownHostException, IOException
 	{
--- ChattyChatChatServer.java
+++ ChattyChatChatServer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 import java.net.*;
 
-public class ChattyServer {
+public class ChattyChatChatServer {
 	
 	static Vector<ClientHandler> activeClientList = new Vector<>();
 	
