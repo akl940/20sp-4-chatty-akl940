d9365b112c2fbf56bdee8746bc4830f6c57cd587
akl940 <59671045+akl940@users.noreply.github.com>
2020-04-07 15:41:05 -0400

    apr 7 first commit

---
+++ ChattyClient.java
@@ -0,0 +1,95 @@
+import java.io.*; 
+import java.net.*; 
+import java.util.Scanner; 
+
+public class ChattyClient {
+
+	public static void main(String args[]) throws UnknownHostException, IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
+		
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		
+		SendClientMsg scm = new SendClientMsg(output, sc);
+		ReadClientMsg rcm = new ReadClientMsg(input);
+		
+		Thread sendT = new Thread(scm);
+		Thread readT = new Thread(rcm);
+		
+		sendT.start();
+		readT.start();
+	}
+	
+	public void stopIO(DataInputStream input, DataOutputStream output, Socket socket) //close resources 
+    {
+        try
+        { 
+            input.close();
+            output.close();
+            socket.close();
+        }
+        catch(IOException e)
+        {  e.printStackTrace(); }    
+    }
+}
+
+class SendClientMsg implements Runnable //send from client to server
+{
+	final DataOutputStream output;
+	final Scanner sc;
+	
+	public SendClientMsg(DataOutputStream output, Scanner sc)
+	{
+		this.output = output;
+		this.sc = sc;
+	}
+	
+	@Override
+	public void run()
+	{
+		while(true)
+		{
+			String msg = sc.nextLine();
+			try
+			{
+				output.writeUTF(msg);
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+	}
+}
+
+class ReadClientMsg implements Runnable //read message sent to this client
+{
+	final DataInputStream input;
+	
+	public ReadClientMsg(DataInputStream input)
+	{
+		this.input = input;
+	}
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			String msg;
+			while(true)
+			{
+				msg = input.readUTF(); //read message
+				System.out.println(msg); //display message
+			}
+		}
+		catch(IOException e)
+		{
+			System.exit(0);
+		}
+	}
+}
+
---
+++ ChattyServer.java
@@ -0,0 +1,149 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class ChattyServer {
+	
+	static Vector<ClientHandler> activeClientList = new Vector<>();
+	
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]));
+		//^^listen on port # entered in
+		
+		Socket socket; //for clients to connect to
+		
+		while(true) //running server
+		{
+			socket = ss.accept(); //accept request
+			
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			
+			
+			//for current client
+			ClientHandler client = new ClientHandler(socket, input, output);
+			Thread t = new Thread(client);
+			activeClientList.add(client);
+			t.start();
+		}
+	}
+}
+
+class ClientHandler implements Runnable //handles client input
+{
+	String name = "Anonymous"; //anonymous nickname unless/until user manually sets their own 
+	final DataInputStream is;
+	final DataOutputStream os;
+	Socket socket;
+	
+	public ClientHandler(Socket socket, DataInputStream is, DataOutputStream os)
+	{
+		this.is = is;
+		this.os = os;
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run()
+	{
+		String line;
+		while(true) //listen for client input
+		{
+			try
+			{
+				line = is.readUTF();
+				System.out.println(name + ": " + line);
+				
+				if(line.equals("/quit")) //user quits program
+				{
+					quitChat(); 
+					break;
+				}
+				if(line.indexOf("/nick") >= 0)
+					this.setNickname(parseMsg(line));
+				else if(line.indexOf("/dm") >= 0)
+					this.sendDM(parseMsg(line));
+				else
+					sendNormal(line);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public String[] parseMsg(String line)
+	{
+		String[] parsed = line.split(" ");
+		return parsed;
+	}
+	
+	//quit
+	public void quitChat() //close IO
+	{
+		try
+		{
+			this.is.close();
+			this.os.close();
+			this.socket.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	//nick
+	public void setNickname(String[] parsed)
+	{
+		this.name = parsed[1];
+	}
+	
+	//dm
+	public void sendDM(String[] parsed)
+	{
+		try
+		{
+			for (ClientHandler client : ChattyServer.activeClientList)
+				dmHelper(client, parsed);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void dmHelper(ClientHandler client, String[] parsed) throws IOException
+	{
+		if(client.name.equals(parsed[1]))
+		{
+			parsed = actualMessageArray(parsed); //create new array to relay actual message without /dm and nickname
+			String message = String.join(" ", parsed); //make into a single string
+			client.os.writeUTF(this.name + ": " + message); //display message to client
+		}
+	}
+	public String[] actualMessageArray(String[] array)
+	{
+		String[] newArray = new String[array.length-2];
+
+        for(int i = 0; i < array.length-2; i++)
+            newArray[i] = array[i+2]; //copy old to new String[]
+            
+        return newArray;
+	}
+	
+	//no commands, just regular message to all
+	public void sendNormal(String line)
+	{
+		try
+		{
+			for (ClientHandler client : ChattyServer.activeClientList)
+				client.os.writeUTF(this.name + ": " + line);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
